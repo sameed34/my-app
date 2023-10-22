8235576fd6108363640b6d6572eebd69b77e38e9
sameedabbas <148718781+sameedabbas@users.noreply.github.com>
2023-10-23 01:34:07 +0500

    updated Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,7 +24,7 @@ export default function Form(props) {
     <h1 style={{color: props.mode==="light"?"black":"white"}}>{props.heading}</h1>
   <textarea className="form-control my-2" style={{backgroundColor: props.mode==="dark"?"grey":"white", color: props.mode==="light"?"black":"white"}} value={text} onChange={cha} id="exampleFormControlTextarea1" rows="8"></textarea>
   <button className="btn btn-primary mx-2" onClick={up}>Convert to Uppercase updated</button>
-  <button className="btn btn-primary" onClick={low}>Convert to Lowercase</button>
+  <button className="btn btn-primary" onClick={low}>Convert to Lowercase updated</button>
 
 </div>
 <div className="container my-15" style={{color: props.mode==="light"?"black":"white"}} >
